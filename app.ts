950029889e4e41718b9dfcf5a275f841cbb73d3e
Natalya Lebedeva <natashaswan.ca@gmail.com>
2022-12-28 00:28:41 -0800

    basic types learned

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('app.js is working');
+//Types
+
+function sum (num1: number, num2: number){
+    return num1 + num2;
+}
+console.log(sum(3, 4))
+console.log(sum(9, 4));
+
+function twoStrings(str1: string, str2: string){
+    return str1 + str2
+}
+
+console.log(twoStrings(5, 'lolo'))
