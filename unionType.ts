d0961def536843f3f021b3857e558eb82bd08a82
Natalya Lebedeva <natashaswan.ca@gmail.com>
2022-12-30 23:50:03 -0800

    learning literal types

--- unionType.ts
+++ unionType.ts
@@ -6,4 +6,16 @@ const combine = (first: number | string, second: number | string) => {
 }
 console.log(combine(4, 5));
 console.log(combine('4', '5'));
+//adding type conversion
 
+const combineOne = (first: number | string, second: number | string, resultType: string) => {
+    let result;
+    if(typeof first === 'number' && typeof second === 'number') result = first + second;
+    else result = first.toString() + second.toString();
+    if(resultType === 'text'){
+        return result.toString()
+    }
+    else return +result;
+}
+console.log(combineOne(4, 5, 'text'));
+console.log(combineOne('4', '5', 'number'));
