c3c583a66be3f15579321fef2602ab7113677b9d
Natalya Lebedeva <natashaswan.ca@gmail.com>
2023-01-02 23:45:55 -0800

    create extended class

--- classes.ts
+++ classes.ts
@@ -1,9 +1,11 @@
 class Department {
     departmentName: string;
+    id: string;
     private employees: string[] = [];
 
-    constructor(n: string){
-        this.departmentName = n;
+    constructor(id: string, name: string){
+        this.id = id;
+        this.departmentName = name;
     }
 
     addEmployee(obj: {name: string; lastName: string}){
@@ -11,21 +13,33 @@ class Department {
     }
 
 }
-const newDepartment = new Department('Accounting');
+const newDepartment = new Department('129', 'Accounting');
 newDepartment.addEmployee({name: 'Max', lastName: 'Jonson'})
 newDepartment.addEmployee({name: 'John', lastName: 'Jonson'})
 //this.employees.push({name: 'Test', lastName: 'Test'}) brings error as it should
 console.log(newDepartment)
 
-//Inheitance
+//Inheritance
 class ITDepartment extends Department{
     private administrators: string[] = [];
-
+    constructor(id: string){
+        super(id, 'IT Department')
+    }
     addAministrator(obj: {name: string; lastName: string}): void{
         this.administrators.push(obj);
     }
 }
-const ourIT = new ITDepartment('IT Department');
+const ourIT = new ITDepartment('1346');
 ourIT.addAministrator({name: 'Ann', lastName: 'Smith'});
 
+class AccountingDepartment extends Department{
+    reports: string[] = [];
+    constructor(id: string){
+        super(id, 'Accounting Department')
+    }
+}
+
+const ourAccounting = new AccountingDepartment('136');
+
 console.log(ourIT)
+console.log(ourAccounting)
